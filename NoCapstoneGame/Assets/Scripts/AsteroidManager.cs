8e6e70bef787485089909393b762a1d040b070a9
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-04-18 23:36:39 -0600

    basic but repeating asteroid generation
    - has a coroutine where every few seconds it prints to the log to spawn an asteroid
    - generation time is able to be set in the editor

--- NoCapstoneGame/Assets/Scripts/AsteroidManager.cs
+++ NoCapstoneGame/Assets/Scripts/AsteroidManager.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class AsteroidManager : MonoBehaviour
 {
+    [SerializeField] bool init = false;
+       
+    [Header("Generation timers and probabilities")]
+    [Tooltip("the amount of time between when asteroids generate")]
+    [SerializeField] private float generationTime;
 
-    public bool init = false;
+    [SerializeField] private bool generatingAsteroids;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GenerateAsteroids());
     }
 
     // Update is called once per frame
@@ -23,5 +30,20 @@ public class AsteroidManager : MonoBehaviour
             //- or maybe at any given point there's a chance of spawning an asteroid
         }
         
+    }
+
+    private IEnumerator GenerateAsteroids()
+    {
+        while (generatingAsteroids)
+        {
+            float counter = 0f;
+            while (counter < generationTime)
+            {
+                counter += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            
+            Debug.Log("here is where we will spawn an asteroid");
+        }
     }
 }
