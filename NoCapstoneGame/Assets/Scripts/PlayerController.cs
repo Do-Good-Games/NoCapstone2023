3df42ed4ef26b3a57bf4dc0be3e1ec24231d7e26
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2023-05-04 18:08:08 -0600

    Added placeholder collision

--- NoCapstoneGame/Assets/Scripts/PlayerController.cs
+++ NoCapstoneGame/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,22 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerBody;
     [SerializeField] Camera mainCamera;
+    [SerializeField] string hazardLayer;
 
     public void UpdatePosition(InputAction.CallbackContext context)
     {
+        // converts cursor position (in screen space) to world space based on camera position/size
         Vector2 cursorPos = context.ReadValue<Vector2>();
         Vector2 position = mainCamera.ScreenToWorldPoint(cursorPos);
         playerBody.transform.position = position;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(hazardLayer))
+        {
+            //Placeholder, should notify game manager
+            Debug.Log("Player hit");
+        }
     }
 }
