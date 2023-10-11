9c47e2616fe6ed6fbd96ae3a0a9aa7ffcf4b1610
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-10-11 12:55:11 -0600

    file reorg - restructured /speedprototyping folder to more general /protyping - moved previous contents of /speedprototyping to /protyping/speed - made /protyping/capstone

---
+++ NoCapstoneGame/Assets/Scripts/Prototyping/Capstone/Scripts/SOCapstoneBase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SOCapstoneBase : ScriptableObject
+{
+
+    abstract virtual public void ChargeStart();
+
+    abstract virtual public void ChargeContinue();
+
+    abstract virtual public void ChargeFinish();
+
+
+
+}
