6c94805a55a3d21e231720926ad557fcf2b6e854
Robert Breglio <43708172+pwnzor79@users.noreply.github.com>
2024-01-20 18:28:31 -0700

    Explosive asteroids no longer drop energy

--- NoCapstoneGame/Assets/Scripts/Entities/Asteroids/ExplosiveAsteroid.cs
+++ NoCapstoneGame/Assets/Scripts/Entities/Asteroids/ExplosiveAsteroid.cs
@@ -9,6 +9,7 @@ public class ExplosiveAsteroid : Asteroid
 
     public override void DestroyAsteroid()
     {
+        this.numDrops = 0;
         //take current position and place an explosion object here
         GameObject explosion = GameObject.Instantiate(explosionPrefab, this.transform.position, this.transform.rotation);
         //Explosion explosion = (Explosion)GameObject.Instantiate(explosionPrefab, this.transform.position, this.transform.rotation);
